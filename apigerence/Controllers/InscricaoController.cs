d4eab2ee8db14956c8d73814e71af023a1309b20
brunohendias <brunodiashen@gmail.com>
2021-05-14 15:31:24 -0300

    busca inscricao filtrada POST

--- apigerence/Controllers/InscricaoController.cs
+++ apigerence/Controllers/InscricaoController.cs
@@ -1,4 +1,5 @@
 ﻿using apigerence.Models.Context;
+using apigerence.Requests;
 using apigerence.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -23,6 +24,41 @@ namespace apigerence.Controllers
                 var query = (
                     from inscricao in _context.Inscricoes
                     select new {
+                        inscricao,
+                        inscricao.Serie.serie,
+                        inscricao.Atencao.atencao,
+                        inscricao.Turno.turno
+                    }
+                ).ToList();
+
+                if (query.Count > 0) Dados = query;
+
+                return MontaRetorno();
+            }
+            catch (Exception e)
+            {
+                return RespErrorLog(e);
+            }
+        }
+
+        [HttpPost("filtro")]
+        public object Filtro([FromBody] InscricaoRequestGet request)
+        {
+            try
+            {
+                msg.success = "Buscamos essas inscrições com sucesso.";
+                msg.fail = "Não encontramos essas inscrições.";
+
+                var query = (
+                    from inscricao in _context.Inscricoes
+                    where inscricao.nome.Contains(request.nome)
+                        || inscricao.cpf == request.cpf
+                        || inscricao.cod_serie == request.cod_serie
+                        || inscricao.cod_turno == request.cod_turno
+                        || inscricao.cod_atencao == request.cod_atencao
+                        || inscricao.email == request.email
+                    select new
+                    {
                         inscricao,
                         inscricao.Serie.serie,
                         inscricao.Atencao.atencao,
---
+++ apigerence/Requests/InscricaoRequestGet.cs
@@ -0,0 +1,12 @@
+﻿namespace apigerence.Requests
+{
+    public class InscricaoRequestGet
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string cpf { get; set; }
+        public long cod_serie { get; set; }
+        public long cod_atencao { get; set; }
+        public long cod_turno { get; set; }
+    }
+}
