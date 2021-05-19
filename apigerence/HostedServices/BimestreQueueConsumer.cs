befbe03fd86b5480c3c3fe3b0f2f5a61d68624ad
brunohendias <brunodiashen@gmail.com>
2021-05-19 11:39:59 -0300

    consumer

---
+++ apigerence/HostedServices/BimestreQueueConsumer.cs
@@ -0,0 +1,78 @@
+﻿using apigerence.Models;
+using apigerence.Models.Context;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace apigerence.HostedServices
+{
+    public class BimestreQueueConsumer : IHostedService
+    {
+        static IQueueClient _queueClient;
+        private readonly IConfiguration _config;
+        private readonly MySqlContext _context;
+
+        public BimestreQueueConsumer(MySqlContext context, IConfiguration config)
+        {
+            _config = config;
+            string serviceBusConnection = _config.GetValue<string>("AzureServiceBus");
+            _queueClient = new QueueClient(serviceBusConnection, "product");
+            _context = context;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("############## Starting Consumer - Queue ####################");
+            ProcessMessageHandler();
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("############## Stopping Consumer - Queue ####################");
+            await _queueClient.CloseAsync();
+            await Task.CompletedTask;
+        }
+
+        private void ProcessMessageHandler()
+        {
+            MessageHandlerOptions messageHandlerOptions = new(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            _queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        private async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine("### Processing Message - Queue ###");
+            Console.WriteLine($"{DateTime.Now}");
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            Bimestre request = JsonSerializer.Deserialize<Bimestre>(message.Body);
+
+            // Dar sequencia com os dados recebidos da fila na variavel request
+            _context.Bimestres.Add(request);
+            _context.SaveChanges();
+
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            ExceptionReceivedContext context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
