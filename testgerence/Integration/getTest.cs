5b2d2ef1f2a20cf33f3849dbd70e27c9c5995b89
brunohendias <brunodiashen@gmail.com>
2021-06-02 11:31:56 -0300

    validar string retorno

--- testgerence/Integration/getTest.cs
+++ testgerence/Integration/getTest.cs
@@ -1,7 +1,4 @@
-﻿using apigerence.Models;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -15,39 +12,48 @@ namespace testgerence.Integration
         public GetTest(WebApplicationFactory<apigerence.Startup> factory) => 
             client = factory.CreateClient();
 
-        [   
-            Theory, InlineData("Turno"), 
-            InlineData("Turma"), InlineData("Serie"),
-            InlineData("Disciplina"), InlineData("Professor"),
-            InlineData("SituacaoAluno"), InlineData("Atencao"),
-            InlineData("Inscricao"), InlineData("Candidato"), 
-            InlineData("Aluno"), InlineData("DadosSerie"), 
-            InlineData("AlunoDisciplina"), InlineData("SerieDisciplina")
+        [
+            Theory,
+            InlineData("Turno", "cod_turno: 1"),
+            InlineData("Turma", "cod_turma: 1"),
+            InlineData("Serie", "cod_serie: 1"),
+            InlineData("Disciplina", "cod_disciplina: 1"),
+            InlineData("Professor", "cod_prof: 1"),
+            InlineData("SituacaoAluno", "cod_situacao: 1"),
+            InlineData("Atencao", "cod_atencao: 1"),
+            InlineData("Inscricao"),
+            InlineData("Candidato", "cod_can: 2"),
+            InlineData("Aluno", "cod_aluno: 5"),
+            InlineData("DadosSerie"),
+            InlineData("AlunoDisciplina", "cod_aluno: 5"),
+            InlineData("SerieDisciplina")
         ]
-        public async Task Check_if_success (string url)
+        public async Task Check_if_success (string url, string result = null)
         {
             HttpResponseMessage response = await client.GetAsync("/api/v1/" + url);
 
-            // Status Code 200-299
-            response.EnsureSuccessStatusCode();
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
 
-            // Verifica o Header (tipo de retorno)
-            Assert.Equal("application/json; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
             Assert.False(response.Content == null);
 
-            // Pega os dados de retorno
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
             string stringResponse = await response.Content.ReadAsStringAsync();
-            
-            // Verifica se encontrou resultado
+
             Assert.False(stringResponse.Length == 0);
 
-            if (url == "Atencao")
-            { // Exemplos
-                List<Atencao> json = JsonConvert.DeserializeObject<List<Atencao>>(stringResponse);
-                Assert.True(json.Count > 0);
-                Assert.True(json[0].cod_atencao == 1);
+            if (result != null)
+            {
+                Assert.Contains(result, stringResponse.Replace("\"", "").Replace(":", ": "));
             }
         }
     }
 }
+
+/*if (url == "Atencao")// Exemplos
+{
+    List<Atencao> json = JsonConvert.DeserializeObject<List<Atencao>>(stringResponse);
+    Assert.True(json.Count > 0);
+    Assert.True(json[0].cod_atencao == 1);
+}*/
