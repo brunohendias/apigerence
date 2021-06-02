08c51c3033b37fb225fd18a11ac5dbc7ddd081bc
brunohendias <brunodiashen@gmail.com>
2021-06-02 17:14:41 -0300

    integration teste, putTest

--- testgerence/Integration/PutTest.cs
+++ testgerence/Integration/PutTest.cs
@@ -6,24 +6,24 @@ using Xunit;
 
 namespace testgerence.Integration
 {
-    public class PostTest : IClassFixture<WebApplicationFactory<apigerence.Startup>>
+    public class PutTest : IClassFixture<WebApplicationFactory<apigerence.Startup>>
     {
         private readonly HttpClient client;
 
         private static readonly string pathbase = "/api/v1/";
 
-        public PostTest(WebApplicationFactory<apigerence.Startup> factory) =>
+        public PutTest(WebApplicationFactory<apigerence.Startup> factory) =>
            client = factory.CreateClient();
 
         [
             Theory,
-            InlineData("Turno", "{ 'turno': 'teste' }"),
-            InlineData("Turma", "{ 'turma': 'teste' }"),
-            InlineData("Serie", "{ 'serie': 'teste' }"),
-            InlineData("Disciplina", "{ 'disciplina': 'teste' }"),
-            InlineData("Professor", "{ 'nom_prof': 'teste' }"),
-            InlineData("SituacaoAluno", "{ 'situacao': 'teste' }"),
-            InlineData("Atencao", "{ 'atencao': 'teste' }")
+            InlineData("Turno", "{ 'cod_turno': 1, 'turno': 'teste' }"),
+            InlineData("Turma", "{ 'cod_turma': 1, 'turma': 'teste' }"),
+            InlineData("Serie", "{ 'cod_serie': 1, 'serie': 'teste' }"),
+            InlineData("Disciplina", "{ 'cod_disciplina': 1, 'disciplina': 'teste' }"),
+            InlineData("Professor", "{ 'cod_prof': 1, 'nom_prof': 'teste' }"),
+            InlineData("SituacaoAluno", "{ 'cod_situacao': 1, 'situacao': 'teste' }"),
+            InlineData("Atencao", "{ 'cod_atencao': 1, 'atencao': 'teste' }")
         ]
         public async Task Check_if_success(string path, string request = null)
         {
@@ -31,7 +31,7 @@ namespace testgerence.Integration
 
             StringContent data = new(request, Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await client.PostAsync(pathbase + path, data);
+            HttpResponseMessage response = await client.PutAsync(pathbase + path, data);
 
             response.EnsureSuccessStatusCode(); // Status Code 200-299
 
@@ -46,7 +46,7 @@ namespace testgerence.Integration
 
             if (request != null)
             {
-                request = request.Replace("{ ", "").Replace(" }", "").Replace(": ", ":");
+                request = request.Replace("{ ", "").Replace(" }", "").Replace(": ", ":").Replace(", ", ",");
                 Assert.Contains(request, result);
             }
         }
